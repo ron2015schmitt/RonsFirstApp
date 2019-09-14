fbe35bbdaadfabbe856b1e27968615588ca23342
Ron Schmitt <Ron.Schmitt@caa.columbia.edu>
2019-09-14 09:22:11 -0400

    day of week

--- app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
+++ app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.rfschmitt.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String mydate;
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat1 = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss aa");
+        SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEE");
+        String datetime1 = dateformat1.format(c.getTime());
+        String dayOfWeek = dayOfWeekFormat.format(c.getTime()).toUpperCase();
+        TextView dayOfWeekTV = (TextView) findViewById(R.id.dayOfWeek);
+        dayOfWeekTV.setText(dayOfWeek);
     }
 }
