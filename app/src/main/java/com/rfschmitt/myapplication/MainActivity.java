00b541b1fcdbfbf6f244aef2fc662308623ac5e1
Ron Schmitt <Ron.Schmitt@caa.columbia.edu>
2019-09-14 14:35:14 -0400

    Layout: day of week, date, temp. Background. Fonts Code: actual date. fake temp

--- app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
+++ app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
@@ -1,43 +1,55 @@
 package com.rfschmitt.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
-
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        String mydate;
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
 
         Calendar c = Calendar.getInstance();
         SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEE");
         String dayOfWeek = dayOfWeekFormat.format(c.getTime()).toUpperCase();
-        TextView dayOfWeekTV = (TextView) findViewById(R.id.dayOfWeek);
+        TextView dayOfWeekTV = findViewById(R.id.dayOfWeek);
         dayOfWeekTV.setText(dayOfWeek);
 
         SimpleDateFormat dayAndMonthFormat = new SimpleDateFormat("MMM dd");
         String dayAndMonth = dayAndMonthFormat.format(c.getTime());
-        TextView dayAndMonthTV = (TextView) findViewById(R.id.dayAndMonth);
+        TextView dayAndMonthTV = findViewById(R.id.dayAndMonth);
         dayAndMonthTV.setText(dayAndMonth);
 
         String currentTemp = "71";
-        TextView temperatureTV = (TextView) findViewById(R.id.temperature);
+        TextView temperatureTV = findViewById(R.id.temperature);
         temperatureTV.setText(currentTemp);
 
         String temperatureDot = "o";
-        TextView temperatureDotTV = (TextView) findViewById(R.id.temperatureDot);
+        TextView temperatureDotTV = findViewById(R.id.temperatureDot);
         temperatureDotTV.setText(temperatureDot);
 
+        String location = "Silver Spring, MD";
+        TextView locationTV = findViewById(R.id.location);
+        locationTV.setText(location);
+
+        String conditions = "Sunny";
+        TextView conditionsTV = findViewById(R.id.conditions);
+        conditionsTV.setText(conditions);
 
     }
+
 }
