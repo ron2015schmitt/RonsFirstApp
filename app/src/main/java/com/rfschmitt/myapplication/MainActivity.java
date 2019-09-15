a554c7b75b005c626b8386e25eb7de976de0ffc5
Ron Schmitt <Ron.Schmitt@caa.columbia.edu>
2019-09-14 22:46:16 -0400

    implemented permission request for course location: works on both emulator and my Galaxy S7

--- app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
+++ app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
@@ -1,19 +1,23 @@
 package com.rfschmitt.myapplication;
 
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import	android.graphics.drawable.Drawable;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +57,64 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView nowImage = findViewById(R.id.imageNow);
         nowImage.setImageResource(R.drawable.ic_moonwithclouds);
+
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.println(Log.INFO,"MainActivity","pos1");
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Log.println(Log.INFO,"MainActivity","pos2");
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                Log.println(Log.INFO,"MainActivity","pos3");
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            Log.println(Log.INFO,"MainActivity","Permission has already been granted");
+
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        String s = "permissions result received: requestCode="+Integer.toString(requestCode);
+        s += "  grantResults.length="+Integer.toString(grantResults.length);
+        Log.println(Log.INFO,"MainActivity",s);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.println(Log.INFO,"MainActivity","Permission granted");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Log.println(Log.INFO,"MainActivity","Permission denied");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 }
