d012c6148611dc53170fb79de694eb56d9b21d09
Ron Schmitt <Ron.Schmitt@caa.columbia.edu>
2019-09-15 11:29:41 -0400

    City and State retrieved, but no auto update.  Will list last updated and is user touches, then update

--- app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
+++ app/src/main/java/com/rfschmitt/myapplication/MainActivity.java
@@ -1,8 +1,17 @@
 package com.rfschmitt.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,14 +19,24 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
-import android.Manifest;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import	android.graphics.drawable.Drawable;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    LocationManager locationManager;
+    Criteria criteria;
+    Context context;
+    String bestProvider;
+    Location location = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        context = getApplicationContext();
 
 
         Calendar c = Calendar.getInstance();
@@ -47,9 +67,9 @@ public class MainActivity extends AppCompatActivity {
         TextView temperatureDotTV = findViewById(R.id.temperatureDot);
         temperatureDotTV.setText(temperatureDot);
 
-        String location = "Silver Spring, MD";
+        String locationString = "Silver Spring, MD";
         TextView locationTV = findViewById(R.id.location);
-        locationTV.setText(location);
+        locationTV.setText(locationString);
 
         String conditions = "Clear";
         TextView conditionsTV = findViewById(R.id.conditions);
@@ -58,60 +78,166 @@ public class MainActivity extends AppCompatActivity {
         ImageView nowImage = findViewById(R.id.imageNow);
         nowImage.setImageResource(R.drawable.ic_moonwithclouds);
 
+        getLocation();
 
+    }
+
+    public void requestPermissionsFromUser() {
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
+                ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            Log.println(Log.INFO,"MainActivity","No permission at this point");
+            Log.println(Log.INFO, "MainActivity", "No permission at this point");
             // Permission is not granted
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                Log.println(Log.INFO,"MainActivity"," Show an explanation to the user");
+                    ACCESS_COARSE_LOCATION)) {
+                Log.println(Log.INFO, "MainActivity", " Show an explanation to the user");
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
             } else {
                 // No explanation needed; request the permission
-                Log.println(Log.INFO,"MainActivity","No explanation needed; request the permission");
+                Log.println(Log.INFO, "MainActivity", "No explanation needed; request the permission");
 
                 ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        new String[]{ACCESS_COARSE_LOCATION},
                         MY_PERMISSIONS_REQUEST_LOCATION);
 
-           }
+            }
         } else {
             // Permission has already been granted
-            Log.println(Log.INFO,"MainActivity","Permission has already been granted");
-
+            Log.println(Log.INFO, "MainActivity", "Location Permission has already been granted");
         }
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
-        String s = "permissions result received: requestCode="+Integer.toString(requestCode);
-        s += "  grantResults.length="+Integer.toString(grantResults.length);
-        Log.println(Log.INFO,"MainActivity",s);
+        String s = "permissions result received: requestCode=" + Integer.toString(requestCode);
+        s += "  grantResults.length=" + Integer.toString(grantResults.length);
+        Log.println(Log.INFO, "MainActivity", s);
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.println(Log.INFO,"MainActivity","Permission granted");
+                    Log.println(Log.INFO, "MainActivity", "Permission granted");
                     // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                    getLocation();
                 } else {
-                    Log.println(Log.INFO,"MainActivity","Permission denied");
+                    Log.println(Log.INFO, "MainActivity", "Permission denied");
                     // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    setDisplayedLocation();
+                    return;
                 }
+
+                // other 'case' lines to check for other
+                // permissions this app might request.
+            }
+        }
+    }
+
+        protected void getLocation() {
+            if (!isLocationPermitted()) {
+                requestPermissionsFromUser();
+            }
+            if (isLocationEnabled(context)) {
+                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                criteria = new Criteria();
+                bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+                // permissions and GPS checked in isLocationEnabled
+                @SuppressLint("MissingPermission") Location newLocation = locationManager.getLastKnownLocation(bestProvider);
+                this.location = newLocation;
+                setDisplayedLocation();
+                if (location != null) {
+                    Log.println(Log.INFO, "GPS", "is ON");
+                }
+                else{
+                    // TODO:
+                    //locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+                }
+            }
+            else {
+
+            }
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //remove location callback:
+//        locationManager.removeUpdates(this);
+
+            //open the map:
+            this.location = location;
+            setDisplayedLocation();
+
+
+        }
+
+        public void setDisplayedLocation() {
+            TextView locationTV = findViewById(R.id.location);
+            if (!isLocationPermitted()) {
+                locationTV.setText("User permissions were not granted for location services");
+                return;
+            }
+            if (location == null) {
+                locationTV.setText("Location (GPS) services unavailable");
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
+            double latitude = location.getLatitude();
+            double longitude = location.getLatitude();
+            //Toast.makeText(MainActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
+            String locationString = Utils.getLocationText(location, context);
+            Log.println(Log.INFO, "GPS", "location="+locationString);
+            locationTV.setText(locationString);
+        }
+
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        public boolean isLocationPermitted() {
+            return (ContextCompat.checkSelfPermission(MainActivity.this, ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED);
+        }
+
+
+        public boolean isLocationEnabled(Context context) {
+            int locationMode = 0;
+            String locationProviders;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                try {
+                    locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+                } catch (Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+
+                return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+            }else{
+                locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+                return !TextUtils.isEmpty(locationProviders);
+            }
+
+
         }
     }
-}
---
+++ app/src/main/java/com/rfschmitt/myapplication/Utils.java
@@ -0,0 +1,234 @@
+package com.rfschmitt.myapplication;
+
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.preference.PreferenceManager;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import android.location.Address;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Utils {
+
+    static final String KEY_REQUESTING_LOCATION_UPDATES = "requesting_locaction_updates";
+
+
+    static private Map<String, String> mStateMap = null;
+
+
+    /**
+     * Returns true if requesting location updates, otherwise returns false.
+     *
+     * @param context The {@link Context}.
+     */
+    static boolean requestingLocationUpdates(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(KEY_REQUESTING_LOCATION_UPDATES, false);
+    }
+
+    /**
+     * Stores the location updates state in SharedPreferences.
+     * @param requestingLocationUpdates The location updates state.
+     */
+    static void setRequestingLocationUpdates(Context context, boolean requestingLocationUpdates) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(KEY_REQUESTING_LOCATION_UPDATES, requestingLocationUpdates)
+                .apply();
+    }
+
+    /**
+     * Returns the {@code location} object as a human readable string.
+     * @param location  The {@link Location}.
+     */
+    static String getLocationCoordsText(Location location) {
+        return location == null ? "Unknown location" :
+                "(" + String.format("%.2f", location.getLatitude()) + ", " + String.format("%.2f", location.getLongitude()) + ")";
+    }
+
+    static String getLocationText(Location location, Context context) {
+        Geocoder geocoder = new Geocoder(context);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            if (geocoder.isPresent()) {
+                StringBuilder stringBuilder = new StringBuilder();
+                if (addresses.size()>0) {
+                    Address address = addresses.get(0);
+
+                    String localityString = address.getLocality();
+                    String name = address.getFeatureName();
+                    String subLocality = address.getSubLocality();
+                    String country = address.getCountryName();
+                    String region_code = address.getCountryCode();
+                    String zipcode = address.getPostalCode();
+                    String state = address.getAdminArea();
+                    String stateAbreviation = getStateAbbreviation(address);
+                    return localityString +", "+stateAbreviation;
+                }
+            } else {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return getLocationCoordsText(location);
+    }
+
+    static public String getStateAbbreviation(Address address) {
+
+        final String TAG = "getStateAbbreviation";
+
+        if (address == null) {
+            return null;
+        }
+
+        populateStates();
+
+        String stateCode = mStateMap.get(address.getAdminArea());
+        if (stateCode == null) {
+            Log.d(TAG, "State mapping failed, parsing from address");
+            stateCode = parseStateCodeFromFullAddress(address);
+            if (stateCode == null) {
+                Log.d(TAG, "Could not parse state from address");
+            }
+        }
+        else {
+            Log.d(TAG, "Successfully mapped " + address.getAdminArea() + " to " + stateCode);
+        }
+
+        return stateCode;
+    }
+
+    static private String parseStateCodeFromFullAddress(Address address) {
+        final String TAG = "parseStateCodeFromFull";
+        if ((address == null) || address.getMaxAddressLineIndex() < 0) {
+            return null;
+        }
+
+        String fullAddress = "";
+        for(int j = 0; j <= address.getMaxAddressLineIndex(); j++) {
+            if (address.getAddressLine(j) != null) {
+                fullAddress += " " + address.getAddressLine(j);
+            }
+        }
+
+        Log.d(TAG, "Full address: " + fullAddress);
+
+        Pattern pattern = Pattern.compile("(?<![A-Za-z0-9])([A-Z]{2})(?![A-Za-z0-9])");
+        Matcher matcher = pattern.matcher(fullAddress);
+
+        String stateCode = null;
+        while (matcher.find()) {
+            stateCode = matcher.group().trim();
+        }
+
+        Log.d(TAG, "Parsed statecode: " + stateCode);
+
+        return stateCode;
+    }
+
+    static void populateStates() {
+        if (mStateMap == null) {
+            mStateMap = new HashMap<String, String>();
+            mStateMap.put("Alabama", "AL");
+            mStateMap.put("Alaska", "AK");
+            mStateMap.put("Alberta", "AB");
+            mStateMap.put("American Samoa", "AS");
+            mStateMap.put("Arizona", "AZ");
+            mStateMap.put("Arkansas", "AR");
+            mStateMap.put("Armed Forces (AE)", "AE");
+            mStateMap.put("Armed Forces Americas", "AA");
+            mStateMap.put("Armed Forces Pacific", "AP");
+            mStateMap.put("British Columbia", "BC");
+            mStateMap.put("California", "CA");
+            mStateMap.put("Colorado", "CO");
+            mStateMap.put("Connecticut", "CT");
+            mStateMap.put("Delaware", "DE");
+            mStateMap.put("District Of Columbia", "DC");
+            mStateMap.put("Florida", "FL");
+            mStateMap.put("Georgia", "GA");
+            mStateMap.put("Guam", "GU");
+            mStateMap.put("Hawaii", "HI");
+            mStateMap.put("Idaho", "ID");
+            mStateMap.put("Illinois", "IL");
+            mStateMap.put("Indiana", "IN");
+            mStateMap.put("Iowa", "IA");
+            mStateMap.put("Kansas", "KS");
+            mStateMap.put("Kentucky", "KY");
+            mStateMap.put("Louisiana", "LA");
+            mStateMap.put("Maine", "ME");
+            mStateMap.put("Manitoba", "MB");
+            mStateMap.put("Maryland", "MD");
+            mStateMap.put("Massachusetts", "MA");
+            mStateMap.put("Michigan", "MI");
+            mStateMap.put("Minnesota", "MN");
+            mStateMap.put("Mississippi", "MS");
+            mStateMap.put("Missouri", "MO");
+            mStateMap.put("Montana", "MT");
+            mStateMap.put("Nebraska", "NE");
+            mStateMap.put("Nevada", "NV");
+            mStateMap.put("New Brunswick", "NB");
+            mStateMap.put("New Hampshire", "NH");
+            mStateMap.put("New Jersey", "NJ");
+            mStateMap.put("New Mexico", "NM");
+            mStateMap.put("New York", "NY");
+            mStateMap.put("Newfoundland", "NF");
+            mStateMap.put("North Carolina", "NC");
+            mStateMap.put("North Dakota", "ND");
+            mStateMap.put("Northwest Territories", "NT");
+            mStateMap.put("Nova Scotia", "NS");
+            mStateMap.put("Nunavut", "NU");
+            mStateMap.put("Ohio", "OH");
+            mStateMap.put("Oklahoma", "OK");
+            mStateMap.put("Ontario", "ON");
+            mStateMap.put("Oregon", "OR");
+            mStateMap.put("Pennsylvania", "PA");
+            mStateMap.put("Prince Edward Island", "PE");
+            mStateMap.put("Puerto Rico", "PR");
+            mStateMap.put("Quebec", "PQ");
+            mStateMap.put("Rhode Island", "RI");
+            mStateMap.put("Saskatchewan", "SK");
+            mStateMap.put("South Carolina", "SC");
+            mStateMap.put("South Dakota", "SD");
+            mStateMap.put("Tennessee", "TN");
+            mStateMap.put("Texas", "TX");
+            mStateMap.put("Utah", "UT");
+            mStateMap.put("Vermont", "VT");
+            mStateMap.put("Virgin Islands", "VI");
+            mStateMap.put("Virginia", "VA");
+            mStateMap.put("Washington", "WA");
+            mStateMap.put("West Virginia", "WV");
+            mStateMap.put("Wisconsin", "WI");
+            mStateMap.put("Wyoming", "WY");
+            mStateMap.put("Yukon Territory", "YT");
+        }
+    }
+}
